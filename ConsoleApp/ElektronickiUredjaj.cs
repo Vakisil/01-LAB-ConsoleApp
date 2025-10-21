8e61de610beca1e90848b162e43fedbb2d12ffb8
Student <Student@DESKTOP-QK9SFGT>
2025-10-21 21:16:37 +0200

    ZAd2(Valentyn Kisil

---
+++ ConsoleApp/ElektronickiUredjaj.cs
@@ -0,0 +1,22 @@
+﻿namespace ElektronickiUredjajiApp
+{
+    public class ElektronickiUredjaj
+    {
+        public string Naziv { get; set; }
+        public string Proizvodjac { get; set; }
+        public int GodinaProizvodnje { get; set; }
+        public ElektronickiUredjaj(string naziv, string proizvodjac, int godinaProizvodnje)
+        {
+            Naziv = naziv;
+            Proizvodjac = proizvodjac;
+            GodinaProizvodnje = godinaProizvodnje;
+        }
+        public virtual void IspisiInformacije()
+        {
+            Console.WriteLine($"Naziv: {Naziv}");
+            Console.WriteLine($"Proizvođač: {Proizvodjac}");
+            Console.WriteLine($"Godina proizvodnje: {GodinaProizvodnje}");
+        }
+    }
+}
+    
---
+++ ConsoleApp/PametniTelefon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class ElektronickiUredjaj
+    {
+        public string Naziv { get; set; }
+        public string Proizvodjac { get; set; }
+        public int GodinaProizvodnje { get; set; }
+
+        public ElektronickiUredjaj(string naziv, string proizvodjac, int godinaProizvodnje)
+        {
+            Naziv = naziv;
+            Proizvodjac = proizvodjac;
+            GodinaProizvodnje = godinaProizvodnje;
+        }
+    }
+    public class PametniTelefon : ElektronickiUredjaj
+    {
+        public string OperativniSustav { get; set; }
+
+        public PametniTelefon(string naziv, string proizvodjac, int godinaProizvodnje, string operativniSustav)
+            : base(naziv, proizvodjac, godinaProizvodnje)
+        {
+            OperativniSustav = operativniSustav;
+        }
+    }
+}
+
+}
